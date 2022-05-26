e565e68f699a554bfa51b7b388dfcad0acb47e3e
Sasu Osayande <osayande.sasu@outlook.com>
2022-05-25 23:32:48 -0400

    Add new routes for the different pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'all', to: 'all#index'
+  get 'edit', to: 'edit#index'
+  get 'new', to: 'new#index'
+  get 'delete', to: 'delete#index'
 end
